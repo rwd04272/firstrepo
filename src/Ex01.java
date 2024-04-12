2d362e1da612e3186fc1bbbe0a90b1c8706424ca
KMS <rwd0427@gmail.com>
2024-04-12 15:47:49 +0900

    두번째 커밋

--- src/Ex01.java
+++ src/Ex01.java
@@ -2,8 +2,10 @@
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("개인 저장소 첫번째 커밋!");
+		System.out.println("두번째 커밋");
+		
 	}
 
 }
